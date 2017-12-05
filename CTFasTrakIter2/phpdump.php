d6755c847bf9f105a045d1a089fa534a42bbdf14
Emily Park <emily.elizabeth.park@my.ccsu.edu>
2017-12-05 17:42:25 -0500

    Adjusted while loop to continuously read, closed php block

--- CTFasTrakIter2/phpdump.php
+++ CTFasTrakIter2/phpdump.php
@@ -14,6 +14,8 @@ $res = $connect -> query("SELECT * FROM Start");
 ?>
 <?php
  header("Content-Type: application/json; charset=UTF-8");
- $obj = json_decode($_GET["routes_json.txt"], false);
+ while ($obj = json_decode($_GET["routes_json.txt"], false)){
  $test = $connect->query("INSERT INTO Routes (route_id, route_short_name, route_type, route_url, route_color, route_text_color)
                           VALUES ($obj->route_id, $obj->route_short_name, $obj->route_type, $obj->route_url, $obj->route_color, $obj->route_text_color)");
+}
+?>
